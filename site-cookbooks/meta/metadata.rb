dc98636aa3d08291349667848e8ca92aa4ad65b8
Mike Fiedler <miketheman@gmail.com>
2013-07-19 00:09:52 -0400

    removing some recipes I don't want

--- site-cookbooks/meta/metadata.rb
+++ site-cookbooks/meta/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  'accounts@pivotallabs.com'
 license           'MIT'
 description       'Uses pivotal_workstation to configure an OSX workstation for ruby development'
 long_description  'Uses pivotal_workstation to configure an OSX workstation for ruby development'
-version           '0.1.0'
+version           '0.1.1'
 supports          'mac_os_x'
 
 depends           'pivotal_workstation'
