2b08a66e36521da99f0f028f97cbeee99ea9d1fa
Mike Fiedler <miketheman@gmail.com>
2013-07-31 11:42:00 -0400

    more of my preferences

---
+++ site-cookbooks/miketheman/recipes/my_preferences.rb
@@ -0,0 +1,32 @@
+# Set clock style
+# {  DateFormat = "EEE MMM d  H:mm";
+#   FlashDateSeparators = :false;
+#   IsAnalog = :false;
+# }
+osx_defaults "set my clock preference" do
+  domain "com.apple.menuextra.clock.plist"
+  key "DateFormat"
+  string "EEE MMM d  H:mm"
+end
+
+# MenuMeters settings.
+# Overwrites the default recipe's file, which is guarded to execute once only
+osx_defaults "set CPU display as Average over multi Procs" do
+  domain "com.ragingmenace.MenuMeters.plist"
+  key "CPUAverageMultiProcs"
+  boolean true
+  notifies :run, 'execute[Restart SystemUIServer]', :delayed
+end
+osx_defaults "set network display as Arrows & Graph" do
+  domain "com.ragingmenace.MenuMeters.plist"
+  key "NetDisplayMode"
+  int 5
+  notifies :run, 'execute[Restart SystemUIServer]', :delayed
+end
+
+# Ask for password 1 minute after screensaver starts
+osx_defaults "set screensaver" do
+  domain "com.apple.screensaver"
+  key "askForPasswordDelay"
+  int 60
+end
