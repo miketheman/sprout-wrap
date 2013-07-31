b144ff2e0c5b9006f3f8817259e5821799c470e8
Mike Fiedler <miketheman@gmail.com>
2013-07-31 11:49:14 -0400

    update theme for user env

--- site-cookbooks/miketheman/recipes/textual_theme.rb
+++ site-cookbooks/miketheman/recipes/textual_theme.rb
@@ -1,5 +1,5 @@
 # Get textual-solarized
-git "~/Library/Containers/com.codeux.irc.textual/Data/Library/Application Support/Textual IRC/Styles/textual-solarized" do
+git "#{ENV['HOME']}/Library/Containers/com.codeux.irc.textual/Data/Library/Application Support/Textual IRC/Styles/textual-solarized" do
   repository "https://github.com/excid3/textual-solarized.git"
   action :sync
 end
